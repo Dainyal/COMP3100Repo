3aed0926c7ada26bf3555a2648421c681d168eee
Dainyal <80859008+Dainyal@users.noreply.github.com>
2023-04-02 21:10:35 +1000

    formatting

--- Assignment1/src/Client.java
+++ Assignment1/src/Client.java
@@ -4,7 +4,7 @@ public class Client {
     //create a socket
     Socket mySocket;
     //initialise input and output streams
-    DataOutputStream outStream;
+    DataOutputStream outputStream;
     BufferedReader inputStream;
     //initialise server and job ID variables
     int serverID = 0;
@@ -14,7 +14,6 @@ public class Client {
     int currentCore = 0;
     int largestCore = 0;
     int serverAmount = 0;
-    int count = 0;
     int largestServerAmount = 0;
     int numJobs = 0;
     boolean firstTime = true;
@@ -34,80 +33,60 @@ public class Client {
 
     public Client(String address, int port) throws Exception{
           mySocket = new Socket(address, port);
-          outStream = new DataOutputStream(mySocket.getOutputStream());
+          outputStream = new DataOutputStream(mySocket.getOutputStream());
           inputStream = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));  
     }
 
     public static void main(String[] args) throws Exception{
         Client c = new Client("192.168.0.69",50000);
         c.byClient();
-        //close socket
         c.mySocket.close();
         c.inputStream.close();
-        c.outStream.close();
+        c.outputStream.close();
     }
-
 
     public void byClient() throws Exception{
         //send HELO to server
         send("HELO");
-        System.out.println("Client message: HELO");
-
         //receive OK
         sMessage = this.inputStream.readLine();
-        System.out.println("Server message: " + sMessage);
-
         //authorise user
         String username = System.getProperty("user.name"); 
         send("AUTH " + username);
-        System.out.println("Client message: AUTH");
-
-        //receive OK
         sMessage = this.inputStream.readLine();
-        System.out.println("Server message: " + sMessage);
-        System.out.println("Before while loop");
-        
-
+ 
         while (!jobType.equals("NONE")) { 
-            System.out.println(" before jobType: " + jobType);
             send("REDY"); 
-            System.out.println("Client message: REDY");
             //server response
-            loopMessage = this.inputStream.readLine();
-            System.out.println("Server message: " + loopMessage); //JOBN 37 0 653 3 700 3800
+            loopMessage = this.inputStream.readLine(); //JOBN 37 0 653 3 700 3800
             arrJobType = loopMessage.split(" ");
-            jobType = arrJobType[0]; //JOBN
+            jobType = arrJobType[0]; 
 
-            if (jobType.equals("JCPL")) {
+            if (jobType.equals("JCPL")) { //If job complete, continue
                 continue;
             }          
 
-            if (firstTime == true) {
+            if (firstTime == true) { //Only on first runthrough
                 firstTime = false;
-                //request server state information
+                //Request server state information
                 send("GETS All");
-                System.out.println("Client message: GETS All"); 
-                //server response
-                strnRecs = this.inputStream.readLine();
-                System.out.println("Server message: string is " + strnRecs); //e.g. DATA nRecs recLen
-                arrstrnRecs = strnRecs.split(" "); //converts to array of strings
-                nRecs = Integer.parseInt(arrstrnRecs[1]); //convert nRecs to int
-                //acknowledge server
+                //Server response
+                strnRecs = this.inputStream.readLine(); //DATA nRecs recLen
+                //Converts to array of strings
+                arrstrnRecs = strnRecs.split(" ");
+                //Convert nRecs to int
+                nRecs = Integer.parseInt(arrstrnRecs[1]); 
+                //Acknowledge server
                 send("OK");
-                System.out.println("Client message: OK");
-                System.out.println("we out here: " + jobType);
-                //xxlarge 0 inactive -1 16
+
                 for (int i = 0; i < nRecs; i++) {
                     sMessage = this.inputStream.readLine();
                     arrsMessage = sMessage.split(" ");
                     currentServerName = arrsMessage[0];
-                    //xxlarge
                     currentServerID = Integer.parseInt(arrsMessage[1]);
-                    //0
                     currentCore = Integer.parseInt(arrsMessage[4]);
-                    //16
                     serverAmount++; 
-
+                    //Replacing largest server type
                     if (!currentServerName.equals(largestServerName)) {
                         if (currentCore > largestCore) {
                             largestCore = currentCore;
@@ -117,33 +96,26 @@ public class Client {
                         }
                     }
                 }          
-                //System.out.println("Largest amount is " + largestServerAmount);
-                send("OK");
-                System.out.println(this.inputStream.readLine());   
-                System.out.println("Jobtype: " + jobType);         
+                send("OK"); 
+                read();    
             } 
             if (jobType.equals("JOBN")) {  
                 send("SCHD " + numJobs + " " + largestServerName + " " + serverID);
-                System.out.println(this.inputStream.readLine());
+                read();
                 numJobs++;       
                 serverID = (serverID + 1)%serverAmount;
             }          
         }
         send("QUIT");
-        System.out.println("Client message: QUIT");
         //receive QUIT
-        System.out.println("Server message: "+ this.inputStream.readLine());
+        read();
+    }
+    //Send Message
+    public void send(String message) throws Exception{
+        this.outputStream.write((message + "\n").getBytes("UTF-8"));
     }
 
-    public void send(String message) throws Exception{
-         this.outStream.write((message + "\n").getBytes("UTF-8"));
-        // this.outStream.flush();
+    public void read() throws Exception {
+        this.inputStream.readLine();
     }
 }
-
-//job: type,id,submitTime,estRunTime,cores,memory,disk
-
-
-//  compile                  javac src\*.java -d bin
-//  run java project         java -cp bin Client
-//  run server               ./ds-server -c ../../configs/sample-configs/ds-sample-config01.xml -n -v all
